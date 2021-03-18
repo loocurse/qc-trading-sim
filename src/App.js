365358ca35699784232e722b86205efef8009a14
Lucas Ng <loocurse@gmail.com>
2021-03-18 11:45:27 +0800

    added paper money

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Buttons from "./components/Buttons";
 import Graph from "./components/Graph";
 
 function App() {
+  const [cash, setCash] = useState(0);
+
+  // data and time are variables used in the graph
   let time = 11;
   const [data, setData] = useState({
     xaxis: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     yaxis: [10, 15, 13, 12, 10, 14, 9, 7, 6, 7, 10],
   });
 
+  // Every 2 seconds, append to data
   useEffect(() => {
     setInterval(() => {
       let x_axis = data.xaxis;
@@ -25,15 +29,17 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
+    <div>
       <nav>
         <h1>Trading portal</h1>
       </nav>
-      <div className="main">
-        <div className="graph">
-          <Graph graphData={data} />
+      <div className="app">
+        <div className="main">
+          <div className="graph">
+            <Graph graphData={data} />
+          </div>
+          <Buttons cash={cash} />
         </div>
-        <Buttons />
       </div>
     </div>
   );
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,9 @@
-function Buttons() {
+function Buttons({cash}) {
     return (
         <div className="buttons">
+            <p>Money: ${cash}</p>
             <button>Buy</button>
             <button>Sell</button>
-            
         </div>
     )
 }
