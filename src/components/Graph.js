e16958935b4d9d7db1c2af4c87931389c6577c82
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2021-03-18 16:03:23 +0800

    Changed type of data. Added initial function to generate data. Changed the way data points are added.

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,24 +1,41 @@
+import React from "react";
 import Chart from "react-apexcharts";
 
-function Graph({ graphData }) {
+const X_AXIS_RANGE = 1000 * 3600 * 24 * 10; // 10 days
 
+function Graph({ graphData }) {
   return (
     <Chart
       options={{
         chart: {
           id: "basic-bar",
+          animations: {
+            enabled: true,
+            easing: "linear",
+            dynamicAnimation: {
+              speed: 2000,
+            },
+          },
+          type: "line",
         },
+        // stroke: {
+        //   curve: "smooth",
+        // },
         xaxis: {
-          categories: graphData.xaxis,
+          range: X_AXIS_RANGE,
+          type: "datetime",
+        },
+        yaxis: {
+          max: 20,
+          min: 0,
         },
       }}
       series={[
         {
           name: "series-1",
-          data: graphData.yaxis,
+          data: graphData.slice(),
         },
       ]}
-      type="line"
       width="600"
     />
   );
