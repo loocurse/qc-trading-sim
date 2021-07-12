ade7301f09da4c51c62ea2e985db53362ce6b1ed
Lucas Ng <lucasngyileang@gmail.com>
2021-07-12 13:19:14 +0800

    Add dynamic ticker choice

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,24 +2,27 @@ import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import TickerList from "./TickerList";
 
-const tickerData = [
-  { symbol: "BABA", market:"NYSE", name: "Alibaba Group Holding Ltd" },
-  { symbol: "LMND", market:"NYSE", name: "Lemonade Inc" },
-  { symbol: "AAPL", market:"NASDAQ", name: "Apple Inc" },
-  { symbol: "TSLA", market:"NASDAQ", name: "Tesla Inc" },
-  { symbol: "GME", market:"NYSE", name: "GameStop Corp." },
-  { symbol: "AMC", market:"NYSE", name: "AMC Entertainment Holdings Inc" },
-  { symbol: "PLTR", market:"NYSE", name: "Palantir Technologies Inc" },
+const defaultTickerData = [
+  { id:1, symbol: "BABA", market:"NYSE", name: "Alibaba Group Holding Ltd" },
+  { id:2, symbol: "LMND", market:"NYSE", name: "Lemonade Inc" },
+  { id:3, symbol: "AAPL", market:"NASDAQ", name: "Apple Inc" },
+  { id:4, symbol: "TSLA", market:"NASDAQ", name: "Tesla Inc" },
+  { id:5, symbol: "GME", market:"NYSE", name: "GameStop Corp." },
+  { id:6, symbol: "AMC", market:"NYSE", name: "AMC Entertainment Holdings Inc" },
+  { id:7, symbol: "PLTR", market:"NYSE", name: "Palantir Technologies Inc" },
 ];
 
 export type ticker = {
 	symbol: string;
 	market: string;
 	name: string;
+  id: number;
 }
 
 function Graph(): JSX.Element {
+  const [tickerData, setTickerData] = useState(defaultTickerData);
   const [tickerList, setTickerList] = useState<ticker[]>(tickerData.slice(0,4));
+  const [selectedTicker, setSelectedTicker] = useState<ticker>(tickerData[0]);
 
   const filterList = (query: string) => {
     if (query) {
@@ -36,7 +39,7 @@ function Graph(): JSX.Element {
 
   return (
     <div>
-      <h2 className="text-3xl font-bold">BABA (NYSE)</h2>
+      <h2 className="text-3xl font-bold">{selectedTicker.symbol} ({selectedTicker.market})</h2>
       <div className="flex justify-between mt-1">
         <div className="flex items-baseline font-bold">
           <h2 className="mr-2 text-xl ">217.20</h2>
@@ -44,7 +47,7 @@ function Graph(): JSX.Element {
         </div>
         <div className="relative">
           <SearchBar filterList={filterList} />
-          <TickerList tickerList={tickerList} />
+          <TickerList tickerData={tickerData} tickerList={tickerList} setSelectedTicker={setSelectedTicker} />
         </div>
       </div>
     </div>
--- src/components/TickerList.tsx
+++ src/components/TickerList.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { ticker } from "./Graph";
 
 type tickerListProps = {
+  tickerData: ticker[];
 	tickerList: ticker[];
+  setSelectedTicker: React.Dispatch<React.SetStateAction<ticker>>;
 }
 
-const TickerList = ({ tickerList }: tickerListProps): JSX.Element => {
+const TickerList = ({ tickerList, setSelectedTicker, tickerData }: tickerListProps): JSX.Element => {
+
+  const clickHandler = (id: number) => {
+    const ticker = tickerData.filter(data => data.id === id)[0];
+    setSelectedTicker(ticker);
+  };
+
   return (
     <div className="dropdown absolute bg-white rounded-lg w-full">
       { tickerList.map((data) => {
         if (data) {
           return (
-            <div className= "p-3 hover:bg-gray-100 cursor-pointer w-full" key={data.name}>
+            <div onClick={() => clickHandler(data.id)} className= "p-3 hover:bg-gray-100 cursor-pointer w-full" key={data.name}>
               <h1 className="text-lg font-bold">{data.symbol}</h1>
               <p className="text-sm">{data.name}</p>
             </div>	
